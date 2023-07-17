9da0ca0aa7b6f01e2255cbe8f91ca3bccdd52eaf
kdev0x <119963213+kdev0x@users.noreply.github.com>
2023-07-17 15:02:54 +0300

    tring neighbors classifier

--- india_covid.py
+++ india_covid.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-
+import KNeighborsClassifier
 from numpy import loadtxt
 from numpy import random
 
@@ -22,7 +22,7 @@ y = data_load[:,8]
 # print(x,y)
 # # print("x:700:"x[700] , 'y:700:' ,'\n', y[700] , '\n' , 'data_load:', data_load[700] ,'\n',"x:600:" ,x[600] , 'y:600:' ,'\n', y[600] , '\n' , 'data_load:', data_load[600])
 
-
+# predction while using tensorflow
 modle = Sequential()
 modle.add(Dense(12 , input_shape = (8,),activation = 'relu'))
 modle.add(Dense(8, activation = 'relu'))
@@ -31,3 +31,16 @@ modle.compile(loss = 'binary_crossentropy' , optimizer = 'adam' , metrics = ['ac
 modle.fit(x,y , epochs = 150, batch_size = 10)
 acc = modle.evaluate(x,y)
 print(acc)
+
+# kneighboor classfire predction
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+k = KNeighborsClassifier(n_neighbors= (12))
+mm = k.fit(x_train, y_train)
+predict1 = mm.predict(x_test)
+
+accuracy = accuracy_score(y_test, predict1)
+print(accuracy)
+
+
+
